b073ac2484cfd6e69f180fe000d850362e824c78
Ankit Jeena <ankitjeena.08@gmail.com>
2024-08-31 00:31:44 +0530

    complete user image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-bffe5.firebaseapp.com",
-  projectId: "mern-blog-bffe5",
-  storageBucket: "mern-blog-bffe5.appspot.com",
-  messagingSenderId: "357021018026",
-  appId: "1:357021018026:web:76969a6dfc5f60c95a2dd3"
+  authDomain: "mern-blog-2eb7f.firebaseapp.com",
+  projectId: "mern-blog-2eb7f",
+  storageBucket: "mern-blog-2eb7f.appspot.com",
+  messagingSenderId: "981651736065",
+  appId: "1:981651736065:web:1d820f6c5a14c6f13ee200"
 };
 
 export const app = initializeApp(firebaseConfig);
