8ff6830e7fda3d186fdc00cbb0cedd46d8f55c28
Ankit Jeena <ankitjeena.08@gmail.com>
2024-08-31 11:20:45 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,13 +16,16 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000 !");
 });
 
-app.use("/api/auth",authRoutes);
+app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
